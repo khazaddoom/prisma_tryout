06b09b075a0be2963ac4973970b093edd2e96408
Ganesh Acharya <ganesh.a@juegostudio.com>
2022-01-31 23:32:52 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient()
+const app = express();
+
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+    const allUsers = await prisma.users.findMany({
+        where: {
+            user_name: {
+                contains: "juego"
+            }
+        }
+    })
+    // console.dir(allUsers, { depth: null })
+    res.json(allUsers)
+})
+
+const server = app.listen(3000, () =>
+  console.log(`🚀 Server ready at: http://localhost:3000`),
+);
